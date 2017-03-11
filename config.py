d80ffeda0d3c367d9603db81491346e4baf1080c
Huangyunbo1996 <hybmail1996@gmail.com>
2017-03-11 20:40:58 +0800

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
     FLASKY_MAIL_SENDER = 'hybstumail@163.com'
-    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN') or 'hybmail1996@qq.com'
+    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     FLASKY_POSTS_PER_PAGE = 10
 
     @staticmethod
@@ -20,8 +20,8 @@ class DevelopmentConfig(Config):
     MAIL_SERVER = 'smtp.163.com'
     MAIL_PORT = 25
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'hybstumail@163.com'
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'Boys740165806'
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URI') or \
     'sqlite:///' + os.path.join(basedir,'data-dev.sqlite')
 
